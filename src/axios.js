decc08435ef9c1cd93eb7bf305d0fb8d65ca7850
Binisha Joshi <bnsajoc@gmail.com>
2024-05-12 17:21:53 +0545

    Axios timeout removed

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
-  timeout: 8000,
   headers: {
     Accept: "application/json",
   },
